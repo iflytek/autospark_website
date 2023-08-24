236fcae1ac3955b12e17ac351bbbbb836a0a67b7
Jonyzqw <a2467449838@163.com>
2023-08-24 02:12:35 +0000

    4.0

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,14 +11,14 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  // url: 'https://your-docusaurus-test-site.com',
+  // // Set the /<baseUrl>/ pathname under which your site is served
+  // // For GitHub pages deployment, it is often '/<projectName>/'
+  // baseUrl: '/',
+  url: 'https://iflytek.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-  // url: 'https://iflytek.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  // baseUrl: '/autospark_website/',
+  baseUrl: '/autospark_website/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
